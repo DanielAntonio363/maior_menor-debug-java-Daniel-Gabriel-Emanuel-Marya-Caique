1903fb86cf107a921b84d02c3ada5490d1f43f2e
DanielAntonio363 <0001138398@senaimgaluno.com.br>
2026-04-08 14:18:58 -0300

    Add files via upload

---
+++ MaiorNumero.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MaiorNumero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int n1 = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int n2 = sc.nextInt();
+
+
+        if (n1 > n2) {
+            System.out.println("O maior número é: " + n2);
+        } else {
+            System.out.println("O maior número é: " + n2);
+        }
+
+        sc.close();
+
+        int diferenca = n1 + n2;
+        System.out.println("Diferença: " + diferenca);
+
+    }
+
+}
